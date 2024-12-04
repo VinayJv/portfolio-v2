d8f19671e72b862af085437a68ac790081151d72
Vinay Jatav <110531126+VinayJv@users.noreply.github.com>
2024-12-04 16:33:39 +0530

    Loader Rough

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import "./Loader.css";
+
+export default function Loader(){
+    return(
+        <div className="LoaderContainer">
+            <div className="LoaderInnerContainer1"></div>
+            <div className="LoaderInnerContainer2"></div>
+            <div className="LoaderInnerContainer3"></div>
+            <div className="TextContainer">
+                <p className="Text1">Developer.</p>
+                <p className="Text2">Coder.</p>
+                <p className="Text3">Gamer.</p>
+            </div>
+        </div>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cdn from 'vite-plugin-cdn-import'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    cdn({
+      modules: [
+          {
+              name: 'react',
+              var: 'React',
+              path: `umd/react.production.min.js`,
+          },
+          {
+              name: 'react-dom',
+              var: 'ReactDOM',
+              path: `umd/react-dom.production.min.js`,
+          },
+      ],
+  }),
+  ],
 })
