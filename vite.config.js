03f8d2a7e07b5131416039cd9b83015fb5d37b84
Vinay Jatav <110531126+VinayJv@users.noreply.github.com>
2024-12-04 17:38:13 +0530

    New Change

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,5 @@ import cdn from 'vite-plugin-cdn-import'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    cdn({
-      modules: [
-          {
-              name: 'react',
-              var: 'React',
-              path: `umd/react.production.min.js`,
-          },
-          {
-              name: 'react-dom',
-              var: 'ReactDOM',
-              path: `umd/react-dom.production.min.js`,
-          },
-      ],
-  }),
-  ],
+  plugins: [react()],
 })
